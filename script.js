ab4b741d4a3f22d20372b9c67473c66bda99852a
Denex A Poulose <107526656+Denex8Poulose@users.noreply.github.com>
2022-08-03 14:52:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+  canvas: document.querySelector('canvas')
+});
+
+
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio)
+
+//create a sphere
+
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(5, 50, 50),
+  new THREE.MeshBasicMaterial({
+    // color:0xff0000
+
+
+    map: new THREE.TextureLoader().load('/MARS/mars.jpeg')
+  })
+)
+
+const light = new THREE.PointLight(0xffffff, 1, 100);
+light.position.set(80, 80, 50);
+
+
+
+scene.add(sphere, light)
+
+
+camera.position.z = 30
+
+
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  sphere.rotation.y += 0.009;
+
+
+
+  renderer.render(scene, camera)
+}
+
+animate();
